d623acafc2220a9d5e9eda3d1986da12131a8cf7
maurice <filiatreaultmaurice@gmail.com>
2022-01-16 16:07:34 -0500

    Added local storage support

--- javascript.js
+++ javascript.js
@@ -1,104 +1,94 @@
 "use strict";
-//
-//This is a array of book Object this array contains all the book created
+const bookForm = document.querySelector(".book-form");
+const overlay = document.querySelector(".overlay");
+const bookContainer = document.querySelector(".book-container");
+const bookFormConfirm = document.querySelector(".form-confirm-btn");
+const changeReadStatusBtn = document.querySelectorAll(".book-read-btn");
 let myLibrary = [];
-//This is a Book constructor to create Book Object
+//Book constructor
 function Book(title, author, page, read) {
   this.title = title;
   this.author = author;
   this.page = page;
   this.read = read;
 }
-//This is a method added to the prototype of the Book constructor wiches mean i will be able to use this method on any Book Object
-Book.prototype.info = function () {
-  return `${this.title} by ${this.author}, ${this.page} pages, ${this.read}`;
-};
-
-//This function allow me to add book to the myLibrary Array each time a new book is called it will call the displayBook function to display it automatiquely
-document.querySelector(".form-confirm-btn").addEventListener("click", addBookToLibrary);
-function addBookToLibrary(e) {
-  e.preventDefault();
-  const title = document.getElementById("title").value;
-  const author = document.getElementById("author").value;
-  const page = document.getElementById("page").value;
-  const read = document.getElementById("read").value;
-  const createBook = new Book(title, author, page, read);
-  myLibrary.push(createBook);
-  document.querySelector(".book-form").reset();
+//Create a new book item
+function createBook() {
+  const book = Object.create(Book.prototype);
+  book.title = document.getElementById("title").value;
+  book.author = document.getElementById("author").value;
+  book.page = document.getElementById("page").value;
+  book.read = document.getElementById("read").value;
+  if (book.title == "" || book.author == "" || book.page == "" || book.read == "") {
+    alert("One or more field are empty");
+    return false;
+  } else return book;
+}
+//addBookToLibrary
+function addBookToLibrary() {
+  saveToLocalStorage();
+}
+bookFormConfirm.addEventListener("click", addBookToLibrary);
+//Change the read status of the book
+Book.prototype.changeReadStatus = function () {
+  this.read == "Read" ? (this.read = "Not read yet") : (this.read = "Read");
+  saveToLocalStorage();
   displayBook();
+};
+//Delete Book
+function addEvent() {
+  //Delete book from library
+  document.querySelectorAll(".book-delete-btn").forEach((btn) => {
+    btn.addEventListener("click", deleteFromLocalStorage);
+  });
 }
 
-//This function display the book on the page by looping trought myLibrary Array
+//Display the book on the page
 function displayBook() {
-  clearBook();
-  for (let i = 0; i < myLibrary.length; i++) {
+  const book = JSON.parse(localStorage.getItem("myLibrary"));
+  bookContainer.innerHTML = "";
+  for (let i = 0; i < book.length; i++) {
     const obj = `<div data-id="${i}" class="card h-100">
-    <div class="card-body">
-      <h5 class="card-title">${myLibrary[i].title}</h5>
-    </div>
-    <ul class="list-group list-group-flush">
-      <li class="list-group-item"><strong>Author:</strong> ${myLibrary[i].author}</li>
-      <li class="list-group-item"><strong>Number of pages:</strong> ${myLibrary[i].page}</li>
-      <li class="list-group-item"><strong>Status:</strong> ${myLibrary[i].read}</li>
-    </ul>
-    <div class="card-button">
-        <button class="book-read-btn btn btn-primary">Read</button>
-        <button class="btn btn-success">Edit</button>
-        <button class="book-delete-btn btn btn-danger">Delete</button>
-    </div>
-  </div>`;
-    document.querySelector(".book-container").insertAdjacentHTML("afterbegin", obj);
+        <div class="card-body">
+          <h5 class="card-title">${book[i].title}</h5>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item"><strong>Author:</strong> ${book[i].author}</li>
+          <li class="list-group-item"><strong>Number of pages:</strong> ${book[i].page}</li>
+          <li class="list-group-item"><strong>Status:</strong> ${book[i].read}</li>
+        </ul>
+        <div class="card-button">
+            <button class="book-read-btn btn btn-primary">Read</button>
+            <button class="btn btn-success">Edit</button>
+            <button class="book-delete-btn btn btn-danger">Delete</button>
+        </div>
+      </div>`;
+    bookContainer.insertAdjacentHTML("afterbegin", obj);
   }
   addEvent();
 }
-
-/**************************************************************************************************************/
-//
-function addEvent() {
-  document.querySelectorAll(".book-delete-btn").forEach((btn) => {
-    btn.addEventListener("click", deleteBook);
-  });
-  document.querySelectorAll(".book-read-btn").forEach((btn) => {
-    btn.addEventListener("click", changeReadStatus);
-  });
+/********************************************************************************************/
+//LocalStorage management
+function saveToLocalStorage() {
+  const book = createBook();
+  if (localStorage.getItem("myLibrary") == null) localStorage.setItem("myLibrary", "[]");
+  let oldData = JSON.parse(localStorage.getItem("myLibrary"));
+  oldData.push(book);
+  localStorage.setItem("myLibrary", JSON.stringify(oldData));
 }
-
-function deleteBook(e) {
+function deleteFromLocalStorage(e) {
   const index = e.currentTarget.closest(".card").dataset.id;
-  myLibrary.splice(index, 1);
+  let oldData = JSON.parse(localStorage.getItem("myLibrary"));
+  oldData = oldData.splice(index, 1);
+  localStorage.setItem("myLibrary", JSON.stringify(oldData));
   displayBook();
 }
-function changeReadStatus(e) {
-  const index = e.currentTarget.closest(".card").dataset.id;
-  if (myLibrary[index].read == "Read") {
-    myLibrary[index].read = "Not read yet";
-  } else {
-    myLibrary[index].read = "Read";
-  }
-  displayBook();
-}
-//
-/**************************************************************************************************************/
-
-// This function is used to clear the page when entering a new book to prevent duplication
-function clearBook() {
-  if (myLibrary.length > -1) {
-    document.querySelector(".book-container").innerHTML = "";
-  }
-}
-// This is the function for the overlay and book form opening and closing
-function toggleOverlayAndForm() {
-  const overlay = document.querySelector(".overlay");
-  const bookForm = document.querySelector(".book-form");
-  if (overlay.classList.contains("hidden") || bookForm.classList.contains("hidden")) {
-    overlay.classList.remove("hidden");
-    bookForm.classList.remove("hidden");
-  } else {
-    overlay.classList.add("hidden");
-    bookForm.classList.add("hidden");
-  }
+//Form toggle
+function toggleOverlayAndForm(e) {
+  e.preventDefault();
+  overlay.classList.toggle("hidden");
+  bookForm.classList.toggle("hidden");
 }
 document.querySelector(".add-new-book-btn").addEventListener("click", toggleOverlayAndForm);
 document.querySelector(".form-cancel-btn").addEventListener("click", toggleOverlayAndForm);
-//This display the book on page load
 displayBook();
