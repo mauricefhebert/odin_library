d24d37979f98157a1f4992c0fbee6afd19995f05
maurice <filiatreaultmaurice@gmail.com>
2022-01-17 12:58:49 -0500

    Almost done with refractoring the odin project step 5/6

--- javascript.js
+++ javascript.js
@@ -1,5 +1,8 @@
 "use strict";
-let myLibrary = JSON.parse(localStorage.getItem("myLibrary"));
+/**********************************************************************************************/
+//Variable
+/**********************************************************************************************/
+let myLibrary = [];
 const bookContainer = document.querySelector(".book-container");
 const changeReadStatusBtn = document.querySelectorAll(".book-read-btn");
 const overlay = document.querySelector(".overlay");
@@ -7,77 +10,86 @@ const bookForm = document.querySelector(".book-form");
 const addNewBookBtn = document.querySelector(".add-new-book-btn");
 const formConfirmBtn = document.querySelector(".form-confirm-btn");
 const formCancelBtn = document.querySelector(".form-cancel-btn");
-//Book constructor
+/**********************************************************************************************/
+//constrcutor
+/**********************************************************************************************/
 function Book(title, author, page, read) {
   this.title = title;
   this.author = author;
   this.page = page;
   this.read = read;
 }
-//Create a new book item
+//Test book
+const hp1 = new Book("Harry Potter 1", "J.K Rowling", 222, "Read");
+const hp2 = new Book("Harry Potter 2", "J.K Rowling", 333, "Not read yet");
+const hp3 = new Book("Harry Potter 3", "J.K Rowling", 443, "Read");
+myLibrary.push(hp1, hp2, hp3);
+/**********************************************************************************************/
+//Static Method
+/**********************************************************************************************/
 function createBook() {
   const book = Object.create(Book.prototype);
   book.title = document.getElementById("title").value;
   book.author = document.getElementById("author").value;
   book.page = document.getElementById("page").value;
   book.read = document.getElementById("read").value;
-  if (book.title == "" || book.author == "" || book.page == "" || book.read == "") {
-    alert("One or more field are empty");
-    return false;
-  } else return book;
+  return book;
 }
-//addBookToLibrary
+
 function addBookToLibrary() {
-  saveToLocalStorage();
+  myLibrary.push(createBook());
+  bookForm.reset();
 }
 formConfirmBtn.addEventListener("click", addBookToLibrary);
-//Change the read status of the book
-Book.prototype.changeReadStatus = function () {
-  this.read == "Read" ? (this.read = "Not read yet") : (this.read = "Read");
-  saveToLocalStorage();
-  displayBook();
-};
 
 //Display the book on the page
 function displayBook() {
   bookContainer.innerHTML = "";
   for (let i = 0; i < myLibrary.length; i++) {
     const obj = `<div data-id="${i}" class="card h-100">
-        <div class="card-body">
-          <h5 class="card-title">${myLibrary[i].title}</h5>
-        </div>
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item"><strong>Author:</strong> ${myLibrary[i].author}</li>
-          <li class="list-group-item"><strong>Number of pages:</strong> ${myLibrary[i].page}</li>
-          <li class="list-group-item"><strong>Status:</strong> ${myLibrary[i].read}</li>
-        </ul>
-        <div class="card-button">
-            <button class="book-read-btn btn btn-primary">Read</button>
-            <button class="btn btn-success">Edit</button>
-            <button class="book-delete-btn btn btn-danger">Delete</button>
-        </div>
-      </div>`;
+          <div class="card-body">
+            <h5 class="card-title">${myLibrary[i].title}</h5>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item"><strong>Author:</strong> ${myLibrary[i].author}</li>
+            <li class="list-group-item"><strong>Number of pages:</strong> ${myLibrary[i].page}</li>
+            <li class="list-group-item"><strong>Status:</strong> ${myLibrary[i].read}</li>
+          </ul>
+          <div class="card-button">
+              <button class="book-read-btn btn btn-primary">Read</button>
+              <button class="btn btn-success">Edit</button>
+              <button class="book-delete-btn btn btn-danger">Delete</button>
+          </div>
+        </div>`;
     bookContainer.insertAdjacentHTML("afterbegin", obj);
   }
+  addEvent();
 }
-/********************************************************************************************/
-//LocalStorage section
-function saveToLocalStorage() {
-  if (localStorage.getItem("myLibrary") == null) localStorage.setItem("myLibrary", "[]");
-  myLibrary.push(createBook());
-  localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
-}
-function deleteFromLocalStorage(e) {
-  const index = e.currentTarget.closest(".card").dataset.id;
-  myLibrary = myLibrary.splice(index, 1);
-  localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+displayBook();
+
+function deleteBook(e) {
+  const bookIndex = e.currentTarget.closest(".card").dataset.id;
+  myLibrary.splice(bookIndex, 1);
   displayBook();
 }
-//Form section
+function addEvent() {
+  document.querySelectorAll(".book-delete-btn").forEach((btn) => {
+    btn.addEventListener("click", deleteBook, false);
+  });
+  document.querySelectorAll(".book-read-btn").forEach((btn) => {});
+}
+/**********************************************************************************************/
+//None Static Method
+/**********************************************************************************************/
+Book.prototype.toggleRead = function () {
+  return this.read == "Read" ? (this.read = "Not read yet") : (this.read = "Read");
+};
+/**********************************************************************************************/
+//Form Manager
+/**********************************************************************************************/
 function toggleOverlayAndForm() {
   overlay.classList.toggle("hidden");
   bookForm.classList.toggle("hidden");
 }
 addNewBookBtn.addEventListener("click", toggleOverlayAndForm);
 formCancelBtn.addEventListener("click", toggleOverlayAndForm);
-displayBook();
